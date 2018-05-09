cf90d477a4ac47033897ae5dd4aa7cca83831486
Angeline Lam <lam.angeline@gmail.com>
2018-05-09 18:16:25 -0400

    almost done

---
+++ dev/scripts/script.js
@@ -0,0 +1,16 @@
+// function ValidateEmail(inputText){
+	// 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	// 	if(inputText.value.match(mailformat))
+	// 		{
+	// 			document.form1.message1.focus();
+	// 			return true;
+	// 		}
+	// 		else
+	// 		{
+	// 			alert("Please enter a valid email address");
+	// 			document.form1.message1.focus();
+	// 			return false;
+	// 		}
+    // 	}
+    
+    
